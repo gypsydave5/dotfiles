f73b87291c0f0ee4ab987eb38dd69ff775dc185a
David Wickes <dave.wickes@gmail.com>
2015-07-14 22:03:46 +0100

    Emacs

--- mac/.emacs.d/init.el
+++ mac/.emacs.d/init.el
@@ -8,6 +8,8 @@
  '(ansi-color-names-vector
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(custom-enabled-themes (quote (wheatgrass)))
+ '(evil-toggle-key "C-M-z")
+ '(package-selected-packages (quote (helm evil)))
  '(send-mail-function (quote smtpmail-send-it))
  '(smtpmail-smtp-server "smtp.gmail.com")
  '(smtpmail-smtp-service 25))
@@ -17,3 +19,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+(require 'package)
+(package-initialize)
+
+; and, just for now, lets set evil mode on...
+(require 'evil)
+(evil-mode 1)
+
+; tabbing between windows...
+(define-key evil-normal-state-map (kbd "TAB") 'evil-window-next)
+
+(require 'helm-config)
+(helm-mode 1)
