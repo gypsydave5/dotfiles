1ceb50dc3b94d5f68be96c4f8982ced16d26d8fc
David Wickes <dave.wickes@gmail.com>
2015-07-20 00:03:50 +0100

    Autocomplete and the evil leader (mwhahahahaaa)

--- mac/.emacs.d/init.el
+++ mac/.emacs.d/init.el
@@ -8,8 +8,14 @@
  '(ansi-color-names-vector
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(custom-enabled-themes (quote (wheatgrass)))
+ '(custom-safe-themes
+   (quote
+    ("f08c2405a7e71e568b784ae0145a86e48e1b4ea8ba33d231a4ad21b52495de5e" "eaf4cb94ad96e1659f9254db8efb799deb1885e97884f8f971ff1e6a4114500a" "356f57a98f35c8ead5a349408cab69f8d4d92baea131e9531611d0d82190fedf" default)))
+ '(electric-pair-mode t)
  '(evil-toggle-key "C-M-z")
- '(package-selected-packages (quote (helm evil)))
+ '(package-selected-packages
+   (quote
+    (dash-at-point monokai-theme ac-slime evil-surround evil-nerd-commenter evil-leader airline-themes powerline-evil powerline helm-ag-r dash ag coffee-mode xpm json-mode image+ graphviz-dot-mode helm-ag golden-ratio elisp-slime-nav slime helm evil)))
  '(send-mail-function (quote smtpmail-send-it))
  '(smtpmail-smtp-server "smtp.gmail.com")
  '(smtpmail-smtp-service 25))
@@ -25,7 +31,13 @@
 (require 'package)
 (package-initialize)
 
-; and, just for now, lets set evil mode on...
+;; autocomplete
+(setq tab-always-indent 'complete)
+(add-to-list 'completion-styles 'initials t)
+
+;; and, just for now, lets set evil mode on...
+(require 'evil-leader)
+(global-evil-leader-mode)
 (require 'evil)
 (evil-mode 1)
 
