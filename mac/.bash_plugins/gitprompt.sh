8b9823b582ff57f6f49c7b6a258476e93a24c1f9
David Wickes <dave.wickes@gmail.com>
2015-09-12 09:49:50 +0100

    Prompt tidy up - removes unused prompt to plugins

---
+++ mac/.bash_plugins/gitprompt.sh
@@ -0,0 +1,37 @@
+#git prompt
+red='\e[0;31m'
+RED='\e[1;31m'
+green='\e[0;32m'
+GREEN='\e[1;32m'
+yellow='\e[0;33m'
+YELLOW='\e[1;33m'
+blue='\e[0;34m'
+BLUE='\e[1;34m'
+magenta='\e[0;35m'
+MAGENTA='\e[1;35m'
+cyan='\e[0;36m'
+CYAN='\e[1;36m'
+NC='\e[0m'              # No Color
+
+function _git_prompt() {
+  local git_status="`git status -unormal 2>&1`"
+  if ! [[ "$git_status" =~ Not\ a\ git\ repo ]]; then
+    if [[ "$git_status" =~ nothing\ to\ commit ]]; then
+      local ansi=$GREEN
+    elif [[ "$git_status" =~ nothing\ added\ to\ commit\ but\ untracked\ files\ present ]]; then
+      local ansi=$RED
+    else
+      local ansi=$YELLOW
+    fi
+    if [[ "$git_status" =~ On\ branch\ ([^[:space:]]+) ]]; then
+      branch=${BASH_REMATCH[1]}
+      #test "$branch" != master || branch=' '
+    else
+      # Detached HEAD.  (branch=HEAD is a faster alternative.)
+      branch="(`git describe --all --contains --abbrev=4 HEAD 2> /dev/null ||
+      echo HEAD`)"
+    fi
+    echo -n '[\['"$ansi"'\]'"$branch"'\[\e[0m\]]'
+  fi
+}
+
