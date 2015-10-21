abced248fee5eed99ab79eee7208d201d73819bf
David Wickes <dave.wickes@gmail.com>
2015-10-21 21:35:24 +0100

    Markdown mode now turns off nlinum

--- mac/.emacs.d/config/my-markdown.el
+++ mac/.emacs.d/config/my-markdown.el
@@ -1,5 +1,6 @@
 (progn
+  (defun markdown-setup ()
+    (nlinum-mode -1))
   (add-hook 'markdown-mode-hook 'auto-fill-mode)
-  (add-hook 'markdown-mode-hook 'flyspell-mode))
-
+  (add-hook 'markdown-mode-hook 'markdown-setup))
 (provide 'my-markdown)
