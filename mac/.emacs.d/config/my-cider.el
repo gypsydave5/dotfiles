861ac0d4bb5f20767d267000f222ca96cbfc6990
David Wickes <dave.wickes@gmail.com>
2015-11-05 12:42:18 +0000

    lispy cider yumO

--- mac/.emacs.d/config/my-cider.el
+++ mac/.emacs.d/config/my-cider.el
@@ -1,12 +1,11 @@
 (progn
-  (add-hook 'clojure-mode-hook 'paredit-mode)
-
   (defun set-auto-complete-as-completion-at-point-function ()
     (setq completion-at-point-functions '(auto-complete)))
 
   (require 'ac-cider)
   (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
   (add-hook 'cider-mode-hook 'ac-cider-setup)
+  (add-hook 'cider-mode-hook 'my-lisp-hook)
   (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
   (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
   (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
