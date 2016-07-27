b195c140754968fea0559da7e63a3dbff01d67e7
David Wickes <dave.wickes@gmail.com>
2016-07-27 20:40:38 +0100

    adds test refresh to the leinigen plugins
    https://github.com/jakemcc/lein-test-refresh

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,1 +1,2 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [com.jakemccrary/lein-test-refresh "0.16.0"]]}}
