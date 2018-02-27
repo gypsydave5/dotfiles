218d430be9fe3b24e19994c411f170076facf36e
David Wickes <dave.wickes@gmail.com>
2018-02-27 08:28:48 +0000

    leinigen

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.15.0"]
+{:user {:plugins [[cider/cider-nrepl "0.16.0-SNAPSHOT"]
                   [com.jakemccrary/lein-test-refresh "0.16.0"]
                   [venantius/ultra "0.5.1"]]
         :test-refresh {:notify-command ["terminal-notifier" "-title" "Tests" "-message"]
