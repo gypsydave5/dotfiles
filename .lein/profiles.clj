2c95a32919760567f0e48cdebde0f1e8120b96c2
David Wickes <dave.wickes@gmail.com>
2019-03-02 18:37:22 +0000

    don't need this lein business

--- .lein/profiles.clj
+++
@@ -1,9 +0,0 @@
-{:user {:plugins [[cider/cider-nrepl "0.16.0-SNAPSHOT"]
-                  [com.jakemccrary/lein-test-refresh "0.16.0"]
-                  [venantius/ultra "0.5.1"]]
-        :test-refresh {:notify-command ["terminal-notifier" "-title" "Tests" "-message"]
-                       :quiet true
-                       :changes-only true}
-        :dependencies [[pjstadig/humane-test-output "0.8.2"]]
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]}}
